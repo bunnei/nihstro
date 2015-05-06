b23069d896ac51b83ccbb169a09c5159fb5b6e95
bunnei <bunneidev@gmail.com>
2015-05-05 22:33:37 -0400

    shader_bytecode: Add instruction decoding for MADI format.

--- include/nihstro/shader_bytecode.h
+++ include/nihstro/shader_bytecode.h
@@ -609,6 +609,16 @@ union Instruction {
 
         BitField<0x18, 0x5, DestRegister> dest;
     } mad;
+
+    union {
+        BitField<0x00, 0x5, uint32_t> operand_desc_id;
+
+        BitField<0x05, 0x7, SourceRegister> src3;
+        BitField<0x0c, 0x5, SourceRegister> src2;
+        BitField<0x11, 0x7, SourceRegister> src1;
+
+        BitField<0x18, 0x5, DestRegister> dest;
+    } madi;
 };
 static_assert(sizeof(Instruction) == 0x4, "Incorrect structure size");
 static_assert(std::is_standard_layout<Instruction>::value, "Structure does not have standard layout");
